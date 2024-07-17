2316d76a601983f09dbbdd4142ba31caac47292c
Enrique <enrique.diaz@alumnos.udg.mx>
2024-07-16 19:24:08 -0600

    Give code format with autopep8 and delete initial comments

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from fastapi import FastAPI
 from models import Todo
 
 app = FastAPI()
-# In-memory array (future database conection)
+
 db = []
 
 @app.get('/')
 def message():
     return "Hello Python!"
-# Show all items in db
 @app.get('/todos/')
 def get_todos():
     return {"Todos": db}
-# Show only one element by id
 @app.get('/todos/{todo_id}')
 def get_todos(todo_id: int):
     for item in db:
@@ -20,19 +18,16 @@ def get_todos(todo_id: int):
             return {"todo": item}
         else:
             return {"message": "Item not found"}
-# Insert a new element in db
 @app.post('/todos/')
 def create_todo(todo: Todo):
     db.append(todo)
-    return {"message": "Todo created!", "todo": todo}
-# Update an element in db by id
+    return {"message": "Todo created!", "todo": todo}    
 @app.put('/todos/{todo_id}')
 def update_todo(todo_id: int, new_todo: Todo):
     if (0 <= todo_id < len(db)):
         db[todo_id] = new_todo
         return {"message": "Item updated", "item": new_todo}
     return {"error": "Item not found"}
-# Delete an element from db by id
 @app.delete('/todos/{todo_id}')
 def delete_todo(todo_id: int):
     if (0 <= todo_id < len(db)):
